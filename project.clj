b182a26ea656658ea065ad913a9fc24833ac8375
ztellman <ztellman@gmail.com>
2010-11-23 10:24:27 -0800

    add license to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject gloss "0.1.0-SNAPSHOT"
   :description "speaks in bytes, so that you don't have to"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.2.0"]
 				 [lamina "0.3.1-SNAPSHOT"]
 				 [potemkin "0.1.0"]])
