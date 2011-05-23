2b5e29652986ccdeb9ac7bc08b14cd42a0709b5b
ztellman <ztellman@gmail.com>
2011-05-22 21:56:59 -0700

    point to lamina 0.2.0-alpha1

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.2.0"]
-				 [lamina "0.4.0-SNAPSHOT"]
+				 [lamina "0.4.0-alpha1"]
 				 [potemkin "0.1.0"]]
   :jvm-opts ["-server"])
