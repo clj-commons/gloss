7fcffd462e39e63d1290f18f47389cd95622058a
Daniel E. Renfer <duck@kronkltd.net>
2011-09-27 18:53:25 -0400

    update dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject gloss "0.2.0-beta1"
+(defproject gloss "0.2.0-beta2-SNAPSHOT"
   :description "speaks in bytes, so that you don't have to"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-  :dependencies [[org.clojure/clojure "1.2.0"]
-				 [lamina "0.4.0-beta1"]
-				 [potemkin "0.1.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [lamina "0.4.0-beta2-SNAPSHOT"]
+                 [potemkin "0.1.0"]]
   :jvm-opts ["-server"])
