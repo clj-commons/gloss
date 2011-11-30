9e40f5caec36ec9237abe5ae56f019d92d04cd04
ztellman <ztellman@gmail.com>
2011-11-30 13:15:47 -0800

    cut 0.2.0

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject gloss "0.2.0-rc2"
+(defproject gloss "0.2.0"
   :description "speaks in bytes, so that you don't have to"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.2.0"]
-				 [lamina "0.4.0-rc2"]
+				 [lamina "0.4.0"]
 				 [potemkin "0.1.0"]]
   :jvm-opts ["-server"])
