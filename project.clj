01686af9fced8df328fb4b67d974c72274709d44
Zach Tellman <ztellman@gmail.com>
2012-01-24 22:37:21 -0800

    bump to 0.2.1-alpha1

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject gloss "0.2.1-SNAPSHOT"
+(defproject gloss "0.2.1-alpha1"
   :description "speaks in bytes, so that you don't have to"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [lamina "0.4.1-SNAPSHOT"]
+                 [lamina "0.4.1-alpha1"]
                  [potemkin "0.1.1"]]
-  :multi-deps {:all [[lamina "0.4.1-SNAPSHOT"]
+  :multi-deps {:all [[lamina "0.4.1-alpha1"]
                      [potemkin "0.1.1"]]
                "1.2" [[org.clojure/clojure "1.2.1"]]}
   :jvm-opts ["-server"])
