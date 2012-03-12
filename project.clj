437b5d3e6fcc6595fbcd1a2b487ca952a733d7e5
Zach Tellman <ztellman@gmail.com>
2012-03-11 22:47:45 -0700

    update to beta1

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject gloss "0.2.1-alpha2-SNAPSHOT"
+(defproject gloss "0.2.1-beta1"
   :description "speaks in bytes, so that you don't have to"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [lamina "0.4.1-alpha1"]
-                 [potemkin "0.1.1"]]
+                 [lamina "0.4.1-beta1"]
+                 [potemkin "0.1.2"]]
   :multi-deps {:all [[lamina "0.4.1-alpha1"]
                      [potemkin "0.1.1"]]
                "1.2" [[org.clojure/clojure "1.2.1"]]}
