0a66825285070ddb91bfadd19b578dfd75305533
Zach Tellman <ztellman@gmail.com>
2012-04-28 12:50:22 +0530

    update a few dependencies

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [lamina "0.4.1-beta1"]
-                 [potemkin "0.1.2"]]
-  :multi-deps {:all [[lamina "0.4.1-alpha1"]
-                     [potemkin "0.1.1"]]
+                 [lamina "0.4.1-rc1"]
+                 [potemkin "0.1.3"]]
+  :multi-deps {:all [[lamina "0.4.1-rc1"]
+                     [potemkin "0.1.3"]]
                "1.2" [[org.clojure/clojure "1.2.1"]]}
   :jvm-opts ["-server"])
