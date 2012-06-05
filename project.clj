1d27d24751db555bf81350726988d661210e2e84
Zach Tellman <ztellman@gmail.com>
2012-06-05 15:33:51 -0700

    mark 0.2.1

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject gloss "0.2.1-rc3"
+(defproject gloss "0.2.1"
   :description "speaks in bytes, so that you don't have to"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [lamina "0.4.1-rc1"]
+                 [lamina "0.4.1"]
                  [potemkin "0.1.3"]]
-  :multi-deps {:all [[lamina "0.4.1-rc2"]
+  :multi-deps {:all [[lamina "0.4.1"]
                      [potemkin "0.1.3"]]
                "1.2" [[org.clojure/clojure "1.2.1"]]}
   :jvm-opts ["-server"])
