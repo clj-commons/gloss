1aac4392171b26b979a413026bfbc890771df5f2
Zach Tellman <ztellman@gmail.com>
2012-08-07 18:58:06 -0700

    mark alpha3

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject gloss "0.2.2-SNAPSHOT"
+(defproject gloss "0.2.2-alpha3"
   :description "speaks in bytes, so that you don't have to"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [lamina "0.5.0-alpha3"]
-                 [potemkin "0.1.3"]]
-  :multi-deps {:all [[lamina "0.5.0-alpha3"]
-                     [potemkin "0.1.3"]]
+                 [lamina "0.5.0-alpha4"]
+                 [potemkin "0.1.5"]]
+  :multi-deps {:all [[lamina "0.5.0-alpha4"]
+                     [potemkin "0.1.5"]]
                "1.2" [[org.clojure/clojure "1.2.1"]]
                "1.3" [[org.clojure/clojure "1.3.0"]]}
   :jvm-opts ["-server"])
