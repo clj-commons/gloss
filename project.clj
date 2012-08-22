1178912d2a1244b304c17803f9a963d91330b1cc
Zach Tellman <ztellman@gmail.com>
2012-08-22 14:17:52 -0700

    mark beta1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gloss "0.2.2-alpha3"
+(defproject gloss "0.2.2-beta1"
   :description "speaks in bytes, so that you don't have to"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
