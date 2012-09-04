288c1aa6ff289c8c68a6ec05f3d5acdda0d600fd
Zach Tellman <ztellman@gmail.com>
2012-09-04 09:13:55 -0700

    back to SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gloss "0.2.2-beta1"
+(defproject gloss "0.2.2-SNAPSHOT"
   :description "speaks in bytes, so that you don't have to"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
@@ -8,6 +8,7 @@
                  [potemkin "0.1.5"]]
   :multi-deps {:all [[lamina "0.5.0-beta1"]
                      [potemkin "0.1.5"]]
+			   "master" [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]
                "1.2" [[org.clojure/clojure "1.2.1"]]
                "1.3" [[org.clojure/clojure "1.3.0"]]}
   :jvm-opts ["-server"])
