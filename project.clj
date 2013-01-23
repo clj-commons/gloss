6acb468727bec22099f22445a38dd4bc2fc42560
Zach Tellman <ztellman@gmail.com>
2013-01-23 15:13:32 -0800

    update readme

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gloss "0.2.2-beta4"
+(defproject gloss "0.2.2-SNAPSHOT"
   :description "speaks in bytes, so that you don't have to"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
