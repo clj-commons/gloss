ab88dd2dcc78b710ab579bd6491aaa774c4e6685
Zach Tellman <ztellman@gmail.com>
2013-08-25 14:01:14 -0700

    mark 0.2.2

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject gloss "0.2.2-SNAPSHOT"
+(defproject gloss "0.2.2"
   :description "speaks in bytes, so that you don't have to"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-  :dependencies [[lamina "0.5.0-rc3"]
-                 [potemkin "0.3.1"]]
+  :dependencies [[lamina "0.5.0"]
+                 [potemkin "0.3.2"]]
   :aliases {"all" ["with-profile" "1.6,dev:1.3,dev:dev:1.4,dev"]}
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
