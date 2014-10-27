c7f8d23634911ea4acdeb9413533275c74adfea8
Daniel Compton <desk@danielcompton.net>
2014-10-28 12:21:16 +1300

    Add trailing }

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [potemkin "0.3.9"]]
   :aliases {"all" ["with-profile" "dev,1.6:dev,1.3:dev:dev,1.4:dev,1.7"]}
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha3"]]
+  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha3"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :dev {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
