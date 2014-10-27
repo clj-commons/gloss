84efc3b981fc5e1bfa1f984c0eb4bac7ce073945
Daniel Compton <desk@danielcompton.net>
2014-10-28 12:11:15 +1300

    Add 1.7-alpha3 test profile

--- project.clj
+++ project.clj
@@ -5,9 +5,10 @@
             :distribution :repo}
   :dependencies [[manifold "0.1.0-alpha4"]
                  [potemkin "0.3.9"]]
-  :aliases {"all" ["with-profile" "1.6,dev:1.3,dev:dev:1.4,dev"]}
+  :aliases {"all" ["with-profile" "1.6,dev:1.3,dev:dev:1.4,dev:dev,1.7"]}
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha3"]]
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :dev {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}}
