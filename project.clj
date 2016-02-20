958eb8d49ea446769da9c839b40dfe0b8a39dffd
Tristan Seligmann <mithrandi@mithrandi.net>
2016-02-20 16:46:59 +0200

    Bump deps and profiles.

--- project.clj
+++ project.clj
@@ -3,13 +3,14 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-  :dependencies [[manifold "0.1.0"]
+  :dependencies [[manifold "0.1.2"]
                  [byte-streams "0.2.0"]
-                 [potemkin "0.3.13"]]
-  :aliases {"all" ["with-profile" "1.6,dev:1.3,dev:dev:1.4,dev"]}
+                 [potemkin "0.4.3"]]
+  :aliases {"all" ["with-profile" "dev:1.9:1.7:1.6:1.5"]}
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :profiles {:1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
-             :dev {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}}
+  :profiles {:1.9 {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}
+             :dev {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
   :jvm-opts ["-server"])
