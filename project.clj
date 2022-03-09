d805fb00c9dc2ca796d706b031f2dbf9f40b8cc9
Matthew Davidson <matthew@modulolotus.net>
2022-03-09 18:44:22 +0800

    Prepare for clj-commons group

--- project.clj
+++ project.clj
@@ -1,24 +1,14 @@
-(defproject gloss "0.2.6"
-  :description "speaks in bytes, so that you don't have to"
+(defproject org.clj-commons/gloss "0.2.7"
+  :description "Speaks in bytes, so that you don't have to"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-  :dependencies [[manifold "0.1.3"]
-                 [byte-streams "0.2.1"]
-                 [potemkin "0.4.3"]]
-  :aliases {"all" ["with-profile" "dev:1.9:1.7:1.6:1.5"]}
-  :plugins [[lein-codox "0.9.4"]
-            [ztellman/lein-cljfmt "0.1.10"]]
+  :dependencies [[manifold/manifold "0.2.3"]
+                 [org.clj-commons/byte-streams "0.2.10"]
+                 [potemkin/potemkin "0.4.5"]]
+
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.3"]]}}
   :cljfmt {:indents {#".*" [[:inner 0]]}}
-  :codox {:source-uri "https://github.com/ztellman/gloss/blob/master/{filepath}#L{line}"
-          :metadata {:doc/format :markdown}
-          :namespaces [gloss.core gloss.io]}
-  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :profiles {:1.9 {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}
-             :dev {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
+
   :global-vars {*warn-on-reflection* true}
-  :jvm-opts ["-server"]
   :javac-options ["-target" "1.8" "-source" "1.8"])
