2a1836f3e7b671204dab93e3f1759cc1792d4949
skynet <skynet-gh@users.noreply.github.com>
2022-04-10 20:05:39 -0400

    Fix gloss.data.bytes.delimited/match-loop eval
    Replace with a version that does not use eval.
    Add a basic unit test for match-loop.

---
+++ test/gloss/data/bytes/delimited_test.clj
@@ -0,0 +1,38 @@
+(ns gloss.data.bytes.delimited-test
+  (:require
+    [clojure.test :refer [deftest is testing]]
+    [gloss.data.bytes.delimited :as delimited])
+  (:import
+    (java.nio ByteBuffer)))
+
+
+
+(deftest match-loop
+  (testing "one"
+    (let [d1 (ByteBuffer/allocate 1)
+          ml (delimited/match-loop 
+               [d1]
+               false)]
+      (is (= [true 1 1]
+             (ml d1 true)))
+      (is (= [false 0 0]
+             (ml d1 false)))))
+  (testing "many"
+    (let [d1 (ByteBuffer/allocate 1)
+          d2 (ByteBuffer/allocate 2)
+          d3 (ByteBuffer/allocate 3)
+          ml (delimited/match-loop 
+               [d3 d2 d1]
+               false)]
+      (is (= [true 1 1]
+             (ml d1 true)))
+      (is (= [true 2 2]
+             (ml d2 true)))
+      (is (= [true 3 3]
+             (ml d3 true)))
+      (is (= [false 0 0]
+             (ml d1 false)))
+      (is (= [false 0 0]
+             (ml d2 false)))
+      (is (= [false 0 0]
+             (ml d3 false))))))
