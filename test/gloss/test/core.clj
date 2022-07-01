4940fa7dea9cac0e1570e82f95d3f7b1c14971d5
Matthew Davidson <matthew@modulolotus.net>
2022-07-01 17:27:07 +0800

    Simplified test
    Asserting puts slowed it down too much to show failure

--- test/gloss/test/core.clj
+++ test/gloss/test/core.clj
@@ -359,23 +359,32 @@
     (is-encoded [0 0 -128] [:int24-le] [-8388608])
     (is-encoded [-128 0 0] [:int24-be] [-8388608])))
 
-(require '[taoensso.timbre :as log])
-
-(deftest test-decode-stream
-  (testing "closing the decoded stream doesn't lose data"
+(deftest test-closing-decode-stream
+  (testing "closing the input stream doesn't lose data"
     (let [num-nums 10]
-      (dotimes [test-count 100]
-        #_(log/info "test #" test-count)
+      (dotimes [_ 100]
         (let [str-frame (string "utf-8")
               in (s/stream 0 (map #(encode str-frame %)))
               out (decode-stream in str-frame)
               test-future (future
                             (dotimes [n num-nums]
-                              (is (= @(s/put! in (str n)) true) "failed to put!"))
+                              @(s/put! in (str n)))         ; do not assert here, too slow!
                             (s/close! in))]
           (is (= (map str (range num-nums))
                  (repeatedly num-nums #(deref (s/take! out ::default)))))
-          #_(Thread/sleep 10)
-          #_(log/info "out closed?" (s/closed? out))
           @test-future
           (is (s/closed? out)))))))
+
+#_
+(deftest orig-test-decode-stream
+  (testing "closing the decoded stream doesn't lose data"
+    (dotimes [test-count 100]
+      (let [str-frame (string "utf-8")
+            in (s/stream 1 (map #(encode str-frame %)))
+            out (decode-stream in str-frame)]
+        (future
+          (dotimes [n 10]
+            @(s/put! in (str n)))
+          (s/close! in))
+        (is (= (map str (range 10))
+               (repeatedly 10 #(deref (s/take! out)))))))))
